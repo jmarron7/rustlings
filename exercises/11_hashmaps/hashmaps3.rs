d1a204e6e6c80f4fa39b5091109ff5e1af493450
Jesus Marron <jesusmarron7@gmail.com>
2025-02-24 00:29:54 -0600

    hashmaps done

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -18,7 +18,7 @@ struct TeamScores {
 fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores = HashMap::<&str, TeamScores>::new();
-
+    
     for line in results.lines() {
         let mut split_iterator = line.split(',');
         // NOTE: We use `unwrap` because we didn't deal with error handling yet.
@@ -26,13 +26,31 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         let team_2_name = split_iterator.next().unwrap();
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
-
+        
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        
+        scores.entry(team_1_name)
+            .and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(TeamScores {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        scores.entry(team_2_name)
+            .and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert(TeamScores {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
-
     scores
 }
 
@@ -43,22 +61,22 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     const RESULTS: &str = "England,France,4,2
 France,Italy,3,1
 Poland,Spain,2,0
 Germany,England,2,1
 England,Spain,1,0";
-
+    
     #[test]
     fn build_scores() {
         let scores = build_scores_table(RESULTS);
-
+        
         assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
-            .into_iter()
-            .all(|team_name| scores.contains_key(team_name)));
+        .into_iter()
+        .all(|team_name| scores.contains_key(team_name)));
     }
-
+    
     #[test]
     fn validate_team_score_1() {
         let scores = build_scores_table(RESULTS);
@@ -66,7 +84,7 @@ England,Spain,1,0";
         assert_eq!(team.goals_scored, 6);
         assert_eq!(team.goals_conceded, 4);
     }
-
+    
     #[test]
     fn validate_team_score_2() {
         let scores = build_scores_table(RESULTS);
